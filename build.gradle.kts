33a2e4429307871fe1806ebafbf7419a13f5bfcc
dvag-renato-kakarigji <dvag-renato-kakarigji@dvag.com>
2021-08-03 15:59:03 +0200

    add open-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.com.google.common.collect.Lists
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 
@@ -15,6 +16,7 @@ plugins {
     kotlin("kapt") version "1.5.20"
     id("org.springframework.boot") version "2.5.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.openapi.generator") version "5.1.1"
     id("org.sonarqube") version "3.3"
 }
 
@@ -35,7 +37,13 @@ repositories {
         url = uri("http://repo.spring.io/release/")
     }
     maven {
+        url = uri("http://nexusint1.id.dvag.com:8081/nexus/content/groups/public/")
+    }
+    maven {
         url = uri("https://repo.maven.apache.org/maven2/")
+    }
+    maven {
+        url = uri("https://maven.pkg.github.com/dvag/rest-security-utils")
     }
     repositories.all {
         if(this is MavenArtifactRepository) {
@@ -51,31 +59,40 @@ dependencyManagement {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.slf4j:log4j-over-slf4j")
+    implementation("com.microsoft.azure:applicationinsights-spring-boot-starter:2.6.3")
+    implementation("org.springframework.boot:spring-boot-starter:2.5.2")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:2.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.20")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    implementation("io.springfox:springfox-swagger2:3.0.0")
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
+    implementation("io.springfox:springfox-spring-web:3.0.0")
+    implementation("io.springfox:springfox-bean-validators:3.0.0")
+    implementation("io.swagger.core.v3:swagger-annotations:2.1.10")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.1")
+    implementation("org.zalando:problem-spring-web:0.26.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.5.2")
+    implementation("org.springframework.boot:spring-boot-starter-aop:2.5.2")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.5.2")
+    implementation("ch.sbb:springboot-graceful-shutdown:2.0.1")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.5.2")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.7.1")
+    implementation("org.slf4j:log4j-over-slf4j:1.7.31")
+    implementation("ch.qos.logback:logback-access:1.2.3")
     implementation("net.logstash.logback:logstash-logback-encoder:6.6")
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("org.zalando:problem-spring-webflux:0.26.2")
-    implementation("org.zalando:logbook-spring-boot-webflux-autoconfigure:2.11.0")
-    implementation("io.swagger:swagger-annotations:1.5.21")
-    implementation("javax.validation:validation-api:2.0.1.Final")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-property:$kotestVersion")
-    testImplementation("io.kotest:kotest-extensions-spring-jvm:4.4.3")
-    testImplementation("org.jacoco:org.jacoco.build:0.8.7")
+    implementation("net.rakugakibox.spring.boot:logback-access-spring-boot-starter:2.7.1")
+    implementation("io.swagger:swagger-annotations:1.6.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.2")
+    testImplementation("org.hamcrest:hamcrest-core:2.2")
+    testImplementation("org.hamcrest:hamcrest-library:2.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("com.intuit.karate:karate-apache:0.9.6")
+    testImplementation("com.intuit.karate:karate-junit5:0.9.6")
+    testImplementation("net.masterthought:cucumber-reporting:5.5.4")
+    testImplementation("commons-io:commons-io:2.11.0")
+    compileOnly("org.springframework.boot:spring-boot-configuration-processor:2.5.2")
 }
 
 tasks.jar {
@@ -86,10 +103,33 @@ tasks.bootJar {
     destinationDirectory.set(buildDir)
 }
 
+tasks.register<GenerateTask>("generateRestApiClient") {
+    generatorName.set("spring")
+    modelNameSuffix.set("Dto")
+    inputSpec.set("${projectDir}/spec/service.yml")
+    outputDir.set("${buildDir}/generated-sources/${project.name}-server-v1")
+    version.set("0.0.1-SNAPSHOT")
+    id.set("${project.name}-server-v1")
+    supportingFilesConstrainedTo.set(Lists.newArrayList())
+    apiPackage.set("com.dvag.${project.name}.build.server.v1.api")
+    invokerPackage.set("com.dvag.${project.name}.build.server.v1")
+    modelPackage.set("com.dvag.${project.name}.build.server.v1.model")
+    generateApiTests.set(false)
+    typeMappings.put("OffsetDateTime","LocalDateTime")
+    importMappings.put("java.time.OffsetDateTime","java.time.LocalDateTime")
+    configOptions.put("dateLibrary", "java8")
+    configOptions.put("enumPropertyNaming", "UPPERCASE")
+    configOptions.put("lombok", "false")
+    configOptions.put("useTags", "true")
+    configOptions.put("validateSpec", "true")
+    configOptions.put("interfaceOnly", "true")
+}
+
 tasks.withType<ProcessResources> {
 //    dependsOn(tasks.findByName("generateGitProperties"))
 //    mustRunAfter(tasks["generateGitProperties"])
-    filesMatching("application.yaml") {
+    duplicatesStrategy=DuplicatesStrategy.INCLUDE
+    filesMatching("application.yml") {
         expand(project.properties)
     }
     filesMatching("logback-spring.xml") {
@@ -105,8 +145,16 @@ tasks.create("ensureResources") {
     }
 }
 
+configure<SourceSetContainer> {
+    named("main") {
+        java.srcDir("$buildDir/generated-sources/${project.name}-server-v1/src/main/java")
+        resources.srcDir("$projectDir/src/main/resources")
+    }
+}
+
 tasks.withType<KotlinCompile> {
     dependsOn(
+        tasks.findByName("generateRestApiClient"),
         tasks.findByName("processResources"),
         tasks.findByName("ensureResources"),
     )
--- src/main/kotlin/com/dvag/mock/rest/BlueController.kt
+++ src/main/kotlin/com/dvag/mock/rest/BlueController.kt
@@ -1,21 +1,21 @@
 package com.dvag.mock.rest
 
-//import com.dvag.mock.build.server.v1.api.BlueApi
-//import com.dvag.mock.build.server.v1.model.BlueDto
-//import io.swagger.annotations.ApiParam
-//import org.springframework.http.ResponseEntity
-//import org.springframework.web.bind.annotation.PathVariable
+import com.dvag.mock.build.server.v1.api.BlueApi
+import com.dvag.mock.build.server.v1.model.BlueDto
+import io.swagger.annotations.ApiParam
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BlueController {
+class BlueController : BlueApi{
 
-//    override fun getBlue(@ApiParam(value = "",required=true) @PathVariable("id") id: String): ResponseEntity<BlueDto>  {
-//
-//        val blueDto = BlueDto().produktId(id)
-//        blueDto.put("ID", id)
-//
-//        return ResponseEntity.ok(blueDto)
-//    }
+    override fun getBlue(@ApiParam(value = "",required=true) @PathVariable("id") id: String): ResponseEntity<BlueDto>  {
+
+        val blueDto = BlueDto().produktId(id)
+        blueDto.put("ID", id)
+
+        return ResponseEntity.ok(blueDto)
+    }
 
 }
